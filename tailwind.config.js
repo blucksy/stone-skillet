663069441aacc66b9d38c1e4465dd75e6d68eebc
Tomás Carlson <34401946+TomasCarlson@users.noreply.github.com>
2025-09-25 14:23:23 -0400

    feature: css

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,18 +18,8 @@ const tailwindConfig = {
   },
   theme: {
     extend: {
-      gridTemplateColumns: {
-        14: "repeat(14, minmax(0, 1fr))",
-        15: "repeat(15, minmax(0, 1fr))",
-        16: "repeat(16, minmax(0, 1fr))",
-        17: "repeat(17, minmax(0, 1fr))",
-        18: "repeat(18, minmax(0, 1fr))",
-        19: "repeat(19, minmax(0, 1fr))",
-      },
-      gridColumn: {
-        "span-13": "span 13 / span 13",
-        "span-14": "span 14 / span 14",
-        "span-15": "span 15 / span 15",
+      colors: {
+        skillet: "#c5e0f2",
       },
     },
   },
